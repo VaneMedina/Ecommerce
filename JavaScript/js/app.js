0977f1ac440de307b74497d922bf3bda07a3a64b
Vane <mv382038@gmail.com>
2021-08-31 22:35:24 -0300

    Sigo con la funcionalidad de buscar productos

--- JavaScript/js/app.js
+++ JavaScript/js/app.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('input');
-const button = document.getElementById('button');
+const search = document.getElementById('button');
 const cards = document.getElementById('cards');
 const items = document.getElementById('items');
 const templateCard = document.getElementById('template-card').content;
@@ -9,6 +9,7 @@ const canvas = document.getElementById('canvas');
 const fragment = document.createDocumentFragment();
 const templateCart = document.getElementById('template-cart').content;
 const notFound = document.getElementById('not-found');
+const showPages = document.getElementById('show-pages');
 let cart = {};
 
 
@@ -26,125 +27,123 @@ const products = [
       "id": 1,
       "title": "Café helado con salsa de chocolate amargo",
       "thumbnailUrl": "images/img1.png",
-      "category": "smartphones"
+      "category": "Café"
     },
     {
       "price": 320,
       "id": 2,
       "title": "Café con leche helado con salsa de chocolate",
       "thumbnailUrl": "images/img2.png",
-      "category": "audifonos"
+      "category": "Café"
     },
     {
       "price": 160,
       "id": 3,
       "title": "Jugo De Té Batido De Café Con Leche",
       "thumbnailUrl": "images/img3.png",
-      "category": "audifonos"
+      "category": "Delicatessen y Tortas"
     },
     {
       "price": 170,
       "id": 4,
       "title": "Frappé oreo con crema batida",
       "thumbnailUrl": "images/img5.png",
-      "category": "audifonos"
+      "category": "Delicatessen y Tortas"
     },
     {
       "price": 520,
       "id": 5,
       "title": "Frappe de chocolate con amareti",
       "thumbnailUrl": "images/img10.png",
-      "category": "monitores"
+      "category": "Delicatessen y Tortas"
     },
     {
       "price": 400,
       "id": 6,
       "title": "Frappé chocolate con kinder",
       "thumbnailUrl": "images/img5.png",
-      "category": "monitores"
+      "category": "Frapuccinos"
     },
     {
       "price": 25,
       "id": 7,
       "title": "Té helado de sabor durazno y naranja",
       "thumbnailUrl": "images/img9.png",
-      "category": "ordenadores"
+      "category": "Frapuccinos"
     },
     {
       "price": 369,
       "id": 7,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img8.png",
-      "category": "ordenadores"
+      "category": "Frapuccinos"
     },
     {
       "price": 450,
       "id": 8,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img8.png",
-      "category": "Smartphones"
+      "category": "Frapuccinos"
     },
     {
       "price": 290,
       "id": 9,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img4.png",
-      "category": "ordenadores"
+      "category": "Frapuccinos"
     },
     {
       "price": 320,
       "id": 10,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img12.png",
-      "category": "ordenadores"
+      "category": "Frapuccinos"
     },
     {
       "price": 200,
       "id": 12,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img13.png",
-      "category": "laptops"
+      "category": "Bakery"
     },
     {
       "price": 210,
       "id": 13,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img14.png",
-      "category": "laptops"
+      "category": "Bakery"
     },
     {
       "price": 500,
       "id": 14,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img11.png",
-      "category": "laptops"
+      "category": "Sándwiches"
     },
     {
       "price": 550,
       "id": 15,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img15.png",
-      "category": "laptops"
+      "category": "Sándwiches"
     },
     {
       "price": 550,
       "id": 15,
       "title": "Té helado de sabor algarroba y cacao",
       "thumbnailUrl": "images/img15.png",
-      "category": "laptops"
+      "category": "Sándwiches"
     }
   ]
-/*BÚSQUEDA */
-
-console.log(products)
 
 
+/*searching*/  //FALTA LIMPIAR EL INPUT
 const filtrarProducts = () =>{
     cards.innerHTML = "";
-    const textInput = input.value.toLowerCase();
+    let textInput = input.value.toLowerCase();
+    if(textInput != ""){
     for(let product of products){
         let title = product.title.toLowerCase();
-        console.log(title)
         if(title.indexOf(textInput) !== -1){
             templateCard.querySelector('h4').textContent = product.title;
             templateCard.querySelector('span').textContent = product.price;
@@ -163,26 +162,39 @@ const filtrarProducts = () =>{
         `
     }
     seeAll();
+    input.value = "";
+    }else{
+        showCards(products)
+    }
 }
 
 
 
 function seeAll(){
-    const showPages = document.getElementById('show-pages');
     const button = document.createElement('button');
     button.textContent = 'ver todo';
-    button.classList.add('btn');
+    button.classList.add('btn-green');
     showPages.appendChild(button);
     button.addEventListener('click', function(){
+        cards.innerHTML = "";
         showCards(products);
         showPages.innerHTML = "";
         notFound.innerHTML = "";
     });
-
 }
 
+search.addEventListener('click', filtrarProducts);
 
-button.addEventListener('click', filtrarProducts);
+
+
+
+
+
+
+
+
+
+
 
 
 
